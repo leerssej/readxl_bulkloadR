b70fc31b5241f72a2717ea9110ff9070c0f0a522
leerssej <jens.leerssen@gmail.com>
2017-03-06 10:41:18 -0800

    bigInt and Round Integerated

--- readxl_convert.R
+++ readxl_convert.R
@@ -12,7 +12,7 @@
 ## Desc: * completely header-free data only bound dataframes
 ## Desc: * completely bound first-instance-header dataframes
 # install.packages("tidyverse", repos = "https://cran.cnr.berkeley.edu")
-
+rm(list=ls())
 
 library(tidyverse)
 library(readxl)
@@ -177,36 +177,49 @@ file_names <-
     list.files(
         path = file_path,
         pattern = paste0("*.", file_extension))
+print("file_names:")
 file_names
+
 
 # generate a list for autoprocessing file tree in gitbash
 file_ <- 
     sapply(file_names, chopem)
 # write my_branch_list to run the git branch building
 write.table(file_, "my_branch_list", na = "", row.names = F, sep = " ", col.names = F, quote = F, eol = " ")
+print("file_:")
 file_
+cat("\n")
 
 # Table of all the columns counts
 Tbl_widths <- 
     bind_cols(data_frame(file_names),
               data.frame(do.call("rbind", lapply(file_names, read_excel_width)))) %>% 
     rename(num_cols = do.call..rbind...lapply.file_names..read_excel_width..)
+print("Tbl_Widths:")
 Tbl_widths
 write.csv(Tbl_widths, "../analysis/Tbl_widths.csv", na = "", row.names = F)
+cat("\n")
+
 
 # Table of all the Headers
 Tbl_headers <- 
     bind_cols(data_frame(file_names),
               data.frame(do.call("rbind", lapply(file_names, read_excel_headers))))
+print("Tbl_headers:")
 glimpse(Tbl_headers)
 write.csv(Tbl_headers, "../analysis/Tbl_headers.csv", na = "", row.names = F)
+cat("\n")
+
 
 # Table of all the Types
 Tbl_types <- 
     bind_cols(data_frame(file_names), 
               data.frame(do.call("rbind", lapply(file_names, read_excel_coltypes))))
+print("Tbl_types:")
 Tbl_types
 write.csv(Tbl_types, "../analysis/Tbl_types.csv", na = "", row.names = F)
+cat("\n")
+
 
 # Excel Specific Summary MetaTables
 ## data Row Counts
@@ -214,23 +227,31 @@ Tbl_dataRowCounts_xlsx <-
     bind_cols(data_frame(file_),
               data.frame(do.call("rbind", lapply(file_names, dataRowCount_excel)))) %>%
     rename(num_dRows = do.call..rbind...lapply.file_names..dataRowCount_excel..)
+print("Tbl_dataRowCounts_xlsx")
 Tbl_dataRowCounts_xlsx
 write.csv(Tbl_dataRowCounts_xlsx, "../analysis/Tbl_dataRowCounts_xlsx.csv", na = "", row.names = F)
+cat("\n")
+
 
 print("Building Excel MetaTables")
 ## AmountTotals summary Excel
 ### List of totals which avoids closure issues
-print("1530,12")
-sumOfColumn_excel("1530_.xlsx")
+# print("1530,12")
+# sumOfColumn_excel("1530_.xlsx")
+print("list_of_sums_xlsx")
 list_of_sums_xlsx <- lapply(file_names, sumOfColumn_excel)
-list_of_sums_xlsx
+glimpse(list_of_sums_xlsx)
 ### Bound Table
+print("Tbl_AmountSums_xlsx")
 Tbl_AmountSums_xlsx <-
     bind_cols(data_frame(file_),
               data.frame(do.call("rbind", list_of_sums_xlsx))) %>% 
     rename(Ttl_Amounts = do.call..rbind...list_of_sums_xlsx.)
+print("Tbl_AmountSums_xlsx")
 Tbl_AmountSums_xlsx
 write.csv(Tbl_AmountSums_xlsx, "../analysis/Tbl_AmountSums_xlsx.csv", na = "", row.names = F)
+cat("\n")
+
 
 print("Constructing the .csv")
 # Launch into csv side of the universe
@@ -246,29 +267,47 @@ Tbl_dataRowCounts_csv <-
     bind_cols(data_frame(file_),
               data.frame(do.call("rbind", lapply(file_, dataRowCount_csv)))) %>% 
     rename(num_dRows = do.call..rbind...lapply.file_..dataRowCount_csv..)
+print("Tbl_dataRowCounts_csv")
 Tbl_dataRowCounts_csv
 write.csv(Tbl_dataRowCounts_csv, "../analysis/Tbl_dataRowCounts_csv.csv", na = "", row.names = F)
+cat("\n")
+
 
 ## AmountTotals summary csv
 ### List of totals which avoids closure issues
-sumOfColumn_csv("0890_")
+# sumOfColumn_csv("0890_")
+print("list_of_sums_xlsx")
 list_of_sums_csv <- lapply(file_, sumOfColumn_csv)
+glimpse(list_of_sums_csv)
 
 ### Bound Table
+print("Tbl_AmountSums_csv")
 Tbl_AmountSums_csv <-
     bind_cols(data_frame(file_),
               data.frame(do.call("rbind", list_of_sums_csv))) %>% 
     rename(Ttl_Amounts = do.call..rbind...list_of_sums_csv.)
+print("Tbl_AmountSums_csv")
 Tbl_AmountSums_csv
 write.csv(Tbl_AmountSums_csv, "../analysis/Tbl_AmountSums_csv.csv", na = "", row.names = F)
+cat("\n")
 
 # Integrity cross check
+## RowCounts CrossCheck
+print("RowCounts Integrity Cross Check:")
+print("csv")
+Tbl_dataRowCounts_csv
+print("xlsx")
+Tbl_dataRowCounts_xlsx
+identical(Tbl_dataRowCounts_csv, Tbl_dataRowCounts_xlsx)
+all.equal(Tbl_dataRowCounts_csv, Tbl_dataRowCounts_xlsx)
+cat("\n")
+
+## AmountSums CrossCheck
+print("AmountSums Integrity Cross Check:")
+print("csv")
 Tbl_AmountSums_csv
+print("xlsx")
 Tbl_AmountSums_xlsx
 identical(Tbl_AmountSums_csv, Tbl_AmountSums_xlsx)
 all.equal(Tbl_AmountSums_csv, Tbl_AmountSums_xlsx)
 
-Tbl_dataRowCounts_csv
-Tbl_dataRowCounts_xlsx
-identical(Tbl_dataRowCounts_csv, Tbl_dataRowCounts_xlsx)
-all.equal(Tbl_dataRowCounts_csv, Tbl_dataRowCounts_xlsx)
