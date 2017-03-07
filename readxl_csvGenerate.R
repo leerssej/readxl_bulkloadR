6c9a6ed3857a522409f0c028041c2327a2a9338d
leerssej <jens.leerssen@gmail.com>
2017-03-07 15:41:51 -0800

    bigint debugged

--- readxl_csvGenerate.R
+++ readxl_csvGenerate.R
@@ -12,14 +12,11 @@
 ## Desc: * completely header-free data only bound dataframes
 ## Desc: * completely bound first-instance-header dataframes
 # install.packages("tidyverse", repos = "https://cran.cnr.berkeley.edu")
-# install.packages("bit64", repos = "https://cran.cnr.berkeley.edu")
+rm(list=ls())
 
 library(tidyverse)
 library(readxl)
-library(bit64)
 
-###### 1. Function Library ######
-## Excel Reading
 read_excel_width <- function(excel_file)
 {
     require("readxl")
@@ -66,9 +63,9 @@ as.double_spprssWrns <- function(variables)
     suppressWarnings(as.double(variables))
 }
 
-as.bigint_spprssWrns <- function(variables) 
+as.integer_spprssWrns <- function(variables) 
 {
-    suppressWarnings(as.integer64(variables))
+    suppressWarnings(as.integer(variables))
 }
 
 # Read in just the data
@@ -78,10 +75,7 @@ read_charxl_data <- function(excel_file)
 {
     dataOnly <- 
         read_charxl_full(excel_file) %>% 
-        select(1:11) %>% 
-        mutate_all(funs(as.numeric)) %>% #some vals disappear in some files if str8 to bigint (see 0920.xlsx)
-        mutate_all(round) %>% 
-        mutate_all(funs(as.integer64)) %>% 
+        mutate_all(funs(as.double_spprssWrns)) %>% 
         filter(!is.na(X11))
     dataOnly
 }
@@ -100,10 +94,8 @@ sumOfColumn_excel <- function(excel_file)
 {
     sum_of_column <- 
         read_charxl_data(excel_file) %>% 
-        select(11) %>% 
-        mutate_all(round) %>% 
+        select(ncol(.)) %>% 
         summarise_all(funs(sum)) %>%
-        mutate_all(funs(as.numeric)) %>% 
         collect %>% 
         .[[1]]
     sum_of_column
@@ -111,35 +103,45 @@ sumOfColumn_excel <- function(excel_file)
 
 # warnings()
 
-csv_convertAllText2bigint <- function (file_)
+Tbl_convertAllText2Numeric <- function (file_)
 {
     read_charxl_full(paste0(file_,".xlsx")) %>%
     select(1:11) %>% 
-    mutate(AmountNumeric = suppressWarnings(as.numeric(.[[ncol(.)]]))) %>% # last col to int
-    mutate(AmountRounded = round(.[[ncol(.)]])) %>% # round last col
+    mutate(AmountDouble = suppressWarnings(as.double(.[[ncol(.)]]))) %>% # last col to int
+    mutate(AmountRoundup = round(.[[ncol(.)]])) %>% # round last col
     select(1:10, ncol(.)) %>% # select 10 + Corrected Amount
-    filter(suppressWarnings(!is.na(AmountRounded))) %>% 
-    mutate_all(funs(as.integer64(.))) %>% 
+    filter(suppressWarnings(!is.na(AmountRoundup))) %>% 
+    mutate_all(funs(as.integer_spprssWrns(.))) %>% 
     write.csv(paste0(file_,".csv"), na = "", row.names = F)
 }
 
+# Tbl_convertAllText2Numeric <- function (file_)
+# {
+#     read_charxl_full(paste0(file_,".xlsx")) %>%
+#     mutate_all(funs(intSpprssWarns)) %>% # convert all cols to ints
+#     select(1:11) %>% # just the columns that we need
+#     filter(suppressWarnings(!is.na(X11))) %>% # drop the headers if they had been there
+#     write.csv(paste0(file_,".csv"), na = "", row.names = F)
+# }
 
-# csv side of universe
 chopem <- function(element) {
     substr(element, 1, 5)
 }
 
-# Read in csv file and extract out only the data
+# csv side of universe
+## Read in csv file and extract out only the data
 read_csv_data <- function(csv_file) 
 {
     csv_data <- 
         read.csv(paste0(csv_file, ".csv"),
                  stringsAsFactors = F,
                  na.strings = c("", " ", "NA")) %>% 
-        # mutate_all(funs(as.integer_spprssWrns)) #regular
-        mutate_all(funs(as.double_spprssWrns)) #oversized
+        mutate_all(funs(as.double_spprssWrns)) %>% 
+        filter_(!is.na(ncol(.)))
     csv_data
 }
+
+# glimpse(read_csv_data("1530_", "AmountRoundup"))
 
 # Use dataOnly frame to generate count of rows of data in file
 dataRowCount_csv <- function(csv_file) 
@@ -155,21 +157,20 @@ sumOfColumn_csv <- function(csv_file)
 {
     sum_of_column <- 
         read_csv_data(csv_file) %>% 
-        select(11) %>% 
+        select(ncol(.)) %>% 
         summarise_all(funs(sum)) %>% 
         collect %>% 
         .[[1]]
     sum_of_column
 }
 
+# sumOfColumn_csv("1530_", "AmountRoundup", "AmountRoundup")
+
 #### Edit File Type HERE ####
 file_extension <- "xlsx"
 ## Test local directory without spaces
-getwd()
 file_path <- "C:/Users/Koyot/Documents/GitHub/readxl_bulkloadR/bulkloadR_staging/"
-# to check what is in the actual LEP
 setwd(file_path)
-getwd()
 
 # names of files to process
 file_names <- 
@@ -178,6 +179,7 @@ file_names <-
         pattern = paste0("*.", file_extension))
 print("file_names:")
 file_names
+
 
 # generate a list for autoprocessing file tree in gitbash
 file_ <- 
@@ -208,6 +210,7 @@ glimpse(Tbl_headers)
 write.csv(Tbl_headers, "../analysis/Tbl_headers.csv", na = "", row.names = F)
 cat("\n")
 
+
 # Table of all the Types
 Tbl_types <- 
     bind_cols(data_frame(file_names), 
@@ -217,95 +220,11 @@ Tbl_types
 write.csv(Tbl_types, "../analysis/Tbl_types.csv", na = "", row.names = F)
 cat("\n")
 
-# Excel Specific Summary MetaTables
-## data Row Counts
-Tbl_dataRowCounts_xlsx <-
-    bind_cols(data_frame(file_),
-              data.frame(do.call("rbind", lapply(file_names, dataRowCount_excel)))) %>%
-    rename(num_dRows = do.call..rbind...lapply.file_names..dataRowCount_excel..)
-warnings()
-print("Tbl_dataRowCounts_xlsx")
-Tbl_dataRowCounts_xlsx
-write.csv(Tbl_dataRowCounts_xlsx, "../analysis/Tbl_dataRowCounts_xlsx.csv", na = "", row.names = F)
-cat("\n")
-
-
-
-print("Building Excel MetaTables")
-## AmountTotals summary Excel
-### List of totals which avoids closure issues
-# print("1530,12")
-# sumOfColumn_excel("1530_.xlsx")
-print("list_of_sums_xlsx")
-list_of_sums_xlsx <- lapply(file_names, sumOfColumn_excel)
-glimpse(list_of_sums_xlsx)
-### Bound Table
-print("Tbl_AmountSums_xlsx")
-Tbl_AmountSums_xlsx <-
-    bind_cols(data_frame(file_),
-              data.frame(do.call("rbind", list_of_sums_xlsx))) %>% 
-    rename(Ttl_Amounts = do.call..rbind...list_of_sums_xlsx.)
-print("Tbl_AmountSums_xlsx")
-Tbl_AmountSums_xlsx
-write.csv(Tbl_AmountSums_xlsx, "../analysis/Tbl_AmountSums_xlsx.csv", na = "", row.names = F)
-cat("\n")
-
 
 print("Constructing the .csv")
 # Launch into csv side of the universe
 ## all columns to text, last column to numeric
 ## the last column is also ceilinged to the integer penny
-sapply(file_, csv_convertAllText2bigint)
-# warnings()
+sapply(file_, Tbl_convertAllText2Numeric)
 
-print("Building csv MetaTables")
-# csv Specific Summary MetaTables
-## data Row Counts
-Tbl_dataRowCounts_csv <-
-    bind_cols(data_frame(file_),
-              data.frame(do.call("rbind", lapply(file_, dataRowCount_csv)))) %>% 
-    rename(num_dRows = do.call..rbind...lapply.file_..dataRowCount_csv..)
-print("Tbl_dataRowCounts_csv")
-Tbl_dataRowCounts_csv
-write.csv(Tbl_dataRowCounts_csv, "../analysis/Tbl_dataRowCounts_csv.csv", na = "", row.names = F)
-cat("\n")
-
-
-## AmountTotals summary csv
-### List of totals which avoids closure issues
-# sumOfColumn_csv("0890_")
-print("list_of_sums_csv")
-list_of_sums_csv <- lapply(file_, sumOfColumn_csv)
-glimpse(list_of_sums_csv)
-
-### Bound Table
-print("Tbl_AmountSums_csv")
-Tbl_AmountSums_csv <-
-    bind_cols(data_frame(file_),
-              data.frame(do.call("rbind", list_of_sums_csv))) %>% 
-    rename(Ttl_Amounts = do.call..rbind...list_of_sums_csv.)
-print("Tbl_AmountSums_csv")
-Tbl_AmountSums_csv
-write.csv(Tbl_AmountSums_csv, "../analysis/Tbl_AmountSums_csv.csv", na = "", row.names = F)
-cat("\n")
-
-# Integrity cross check
-## RowCounts CrossCheck
-print("RowCounts Integrity Cross Check:")
-print("csv")
-Tbl_dataRowCounts_csv
-print("xlsx")
-Tbl_dataRowCounts_xlsx
-identical(Tbl_dataRowCounts_csv, Tbl_dataRowCounts_xlsx)
-all.equal(Tbl_dataRowCounts_csv, Tbl_dataRowCounts_xlsx)
-cat("\n")
-
-## AmountSums CrossCheck
-print("AmountSums Integrity Cross Check:")
-print("csv")
-Tbl_AmountSums_csv
-print("xlsx")
-Tbl_AmountSums_xlsx
-identical(Tbl_AmountSums_csv, Tbl_AmountSums_xlsx)
-all.equal(Tbl_AmountSums_csv, Tbl_AmountSums_xlsx)
-
+warnings()
